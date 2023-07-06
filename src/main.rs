2aa239de984d39c0e815725becba97616f0ec641
Ed Cashin <ed.cashin@acm.org>
2023-07-06 17:53:04 -0400

    Decay the influence of history over time

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ use selection::choose;
 mod selection;
 
 #[derive(Parser, Debug)]
-struct Args {
+pub struct Args {
     participants: PathBuf,
-
     #[arg(long)]
     history: Option<PathBuf>,
-
+    #[arg(long, default_value_t = 10.0)]
+    history_halflife: f64,
     #[arg(long)]
     verbosity: Option<usize>,
 }
@@ -25,7 +25,6 @@ fn non_blanks(path: &Path) -> Result<Vec<String>> {
     let content = fs::read_to_string(path).with_context(|| format!("reading {path:?}"))?;
     Ok(content
         .lines()
-        .into_iter()
         .filter(|i| !i.is_empty())
         .map(|s| s.to_string())
         .collect())
@@ -35,16 +34,15 @@ fn main() -> Result<()> {
     let args = Args::parse();
 
     let participants = non_blanks(&args.participants)?;
-    let history = if let Some(hist_path) = args.history {
-        non_blanks(&hist_path)?
+    let history = if let Some(hist_path) = &args.history {
+        non_blanks(hist_path)?
     } else {
         vec![]
     };
     if participants.is_empty() {
         Err(anyhow!("participant list is empty"))
     } else {
-        let selection =
-            choose(&participants, &history, args.verbosity).context("choosing participant")?;
+        let selection = choose(&participants, &history, &args).context("choosing participant")?;
         println!("{selection}");
         Ok(())
     }
--- src/selection.rs
+++ src/selection.rs
@@ -2,20 +2,42 @@ use anyhow::{anyhow, Context, Result};
 use rand::distributions::WeightedIndex;
 use rand_distr::{Beta, Distribution};
 
-pub fn choose(
-    participants: &[String],
-    history: &[String],
-    verbosity: Option<usize>,
-) -> Result<String> {
+use crate::Args;
+
+fn exponentially_weighted_decay(half_life: f64, time: f64) -> f64 {
+    0.5_f64.powf(time / half_life)
+}
+
+pub fn choose(participants: &[String], history: &[String], args: &Args) -> Result<String> {
     let rng = &mut rand::thread_rng();
     let weights = if history.is_empty() {
         vec![1.0; participants.len()]
     } else {
         let recent = (history.len() as f64).log(2.0) as usize;
+        let decay = history
+            .iter()
+            .enumerate()
+            .map(|(i, _)| {
+                let t = i as f64;
+                exponentially_weighted_decay(args.history_halflife, t)
+            })
+            .collect::<Vec<_>>();
         participants
             .iter()
             .map(|name| {
-                let n_past = history.iter().filter(|i| *i == name).count();
+                let weight_past = history
+                    .iter()
+                    .enumerate()
+                    .filter_map(
+                        |(i, name_i)| {
+                            if name_i == name {
+                                Some(decay[i])
+                            } else {
+                                None
+                            }
+                        },
+                    )
+                    .sum::<f64>();
                 if history
                     .iter()
                     .skip(history.len() - recent)
@@ -26,13 +48,13 @@ pub fn choose(
                 } else {
                     // Beta distribution will lean toward zero weight
                     // the more a participant has been previously selected.
-                    let dist = Beta::new(1_f64, (1 + n_past) as f64).unwrap();
+                    let dist = Beta::new(1_f64, 1_f64 + weight_past).unwrap();
                     dist.sample(rng)
                 }
             })
             .collect()
     };
-    if let Some(v) = verbosity {
+    if let Some(v) = args.verbosity {
         // Print out all the weights when user specifies verbosity > 0
         if v > 0 {
             let info = weights
