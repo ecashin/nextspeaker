3d98d010d62174c9bba3fbb450e6cb0b1ed61602
Ed Cashin <ed.cashin@acm.org>
2023-09-08 19:10:46 -0400

    Move components into module

--- nextspeaker-web/src/main.rs
+++ nextspeaker-web/src/main.rs
@@ -3,14 +3,15 @@ use std::collections::HashMap;
 use anyhow::Result;
 use gloo_console::log;
 use serde::{Deserialize, Serialize};
-use stylist::yew::styled_component;
 use wasm_bindgen::JsValue;
 use web_sys::{HtmlInputElement, HtmlTextAreaElement};
 use yew::prelude::*;
 
+use components::{DismissableText, HistoryHalflife, ModeSelect, Selection, SimulationPanel};
 use localstore::LocalStore;
 use nextspeaker::DEFAULT_HALFLIFE;
 
+mod components;
 mod localstore;
 
 const LOCAL_STATE_SCHEMA_VERSION: &str = "v0.1";
@@ -64,186 +65,6 @@ fn from_lines(text: &str) -> Result<Vec<String>> {
         .filter(|i| !i.is_empty())
         .map(|s| s.to_string())
         .collect())
-}
-
-#[derive(Properties, PartialEq)]
-struct SimulationPanelProps {
-    dismiss: Callback<MouseEvent>,
-    simulate: Callback<MouseEvent>,
-    results: Option<Vec<(String, u64)>>,
-}
-
-#[styled_component]
-fn SimulationPanel(props: &SimulationPanelProps) -> Html {
-    html! {
-        <div class={css!("display: flex; background-color: lightgray; flex-direction: column;")}>
-            <div class={css!("display: flex; flex-flow: row-reverse;")}>
-                <DismissButton onclick={props.dismiss.clone()} />
-                <div class={css!("flex: 1;")} />
-            </div>
-            <div><h2>{"Simulation of Next Choice"}</h2></div>
-            <div>
-                <button onclick={props.simulate.clone()}>
-                    {format!("run simulation {N_SIM} times")}
-                </button>
-                <SimulationResults results={props.results.clone()} />
-            </div>
-        </div>
-    }
-}
-
-#[derive(Properties, PartialEq)]
-struct SimulationResultsProps {
-    results: Option<Vec<(String, u64)>>,
-}
-
-#[styled_component]
-fn SimulationResults(props: &SimulationResultsProps) -> Html {
-    if let Some(results) = &props.results {
-        html! {
-            <table>
-                <tr><th>{"candidate"}</th><th width={"80%"}>{"selection count"}</th></tr>
-                {
-                    results.into_iter().map(|(candidate, count)| {
-                        let pct = 100.0 * (*count as f64 / N_SIM as f64);
-                        let pct = format!("{pct}%");
-                        let bar_style = css!(
-                            min-width: ${pct};
-                            box-sizing: border-box;
-                            flex: 0 0 auto;
-                            color: darkgray;
-                            background-color: darkgray;
-                        );
-                        html! {
-                            <tr key={candidate.clone()}>
-                                <td>{candidate}</td>
-                                <td>
-                                <div
-                                    class={css!("background-color: whitesmoke; display: flex; flex-flow: row; width: 80%;")}
-                                >
-                                    <div class={bar_style}>{"X"}</div>
-                                    <div class={css!("background-color: whitesmoke; flex: 1;")}></div>
-                                </div>
-                                </td>
-                            </tr>
-                        }
-                    }).collect::<Html>()
-                }
-            </table>
-        }
-    } else {
-        html! {
-            <div>
-                {"no results yet"}
-            </div>
-        }
-    }
-}
-
-#[derive(Properties, PartialEq)]
-struct HistoryHalflifeProps {
-    oninput: Callback<InputEvent>,
-    value: f64,
-}
-
-#[styled_component]
-fn HistoryHalflife(props: &HistoryHalflifeProps) -> Html {
-    let value = props.value.to_string();
-    html! {
-        <div>
-            <label for={"hhl2die4"}>{"History halflife:"}</label>
-            <input
-                type={"number"}
-                id={"hhl2die4"}
-                value={value}
-                step={"any"}
-                oninput={props.oninput.clone()}
-            />
-        </div>
-    }
-}
-
-#[derive(Properties, PartialEq)]
-struct DismissButtonProps {
-    onclick: Callback<MouseEvent>,
-}
-
-#[styled_component]
-fn DismissButton(props: &DismissButtonProps) -> Html {
-    html! {
-        <button
-            class={css!("color: red; justify-self: right; align-self: start; height: 1.6rem;")}
-            onclick={props.onclick.clone()}
-        >{"X"}</button>
-    }
-}
-
-#[derive(Properties, PartialEq)]
-struct DismissableTextProps {
-    heading: String,
-    oninput: Callback<InputEvent>,
-    dismiss: Callback<MouseEvent>,
-    text: String,
-}
-
-#[styled_component]
-fn DismissableText(props: &DismissableTextProps) -> Html {
-    html! {
-        <div class={css!("background-color: lightgray; display: grid; width: 90%; padding: 1rem; grid-template-columns: 80% 20%;")}>
-            <Text heading={props.heading.clone()} text={props.text.clone()} oninput={props.oninput.clone()}></Text>
-            <DismissButton onclick={props.dismiss.clone()}></DismissButton>
-        </div>
-    }
-}
-
-#[derive(Properties, PartialEq)]
-struct ModeSelectProps {
-    buttons: Html,
-}
-
-#[styled_component]
-fn ModeSelect(props: &ModeSelectProps) -> Html {
-    html! {
-        <div class={css!("width: 50%; padding: 3rem; margin: 1rem;")}>
-            { props.buttons.clone() }
-        </div>
-    }
-}
-
-#[derive(Properties, PartialEq)]
-struct TextProps {
-    heading: String,
-    oninput: Callback<InputEvent>,
-    text: String,
-}
-
-#[styled_component]
-fn Text(props: &TextProps) -> Html {
-    html! {
-        <div class={css!("width: 80%; margin: 3rem; height: 80%;")}>
-            <h3>{props.heading.clone()}</h3>
-            <textarea value={props.text.clone()} oninput={props.oninput.clone()}></textarea>
-        </div>
-    }
-}
-
-#[derive(Properties, PartialEq)]
-struct SelectionProps {
-    text: Option<String>,
-}
-
-#[function_component]
-fn Selection(props: &SelectionProps) -> Html {
-    if let Some(s) = &props.text {
-        let text = format!("selection: {s}");
-        html! {
-            <div>
-                <p>{text}</p>
-            </div>
-        }
-    } else {
-        html! {}
-    }
 }
 
 fn sorted_counts(counts: HashMap<String, u64>) -> Vec<(String, u64)> {
@@ -408,9 +229,9 @@ impl Component for Model {
         match self.mode {
             Mode::MainView => {
                 html! {
-                    <div class="content-area">
+                    <div>
                         <ModeSelect buttons={mode_select_buttons}></ModeSelect>
-                        <div class="action-area">
+                        <div>
                             <button onclick={onchoose}>{"CHOOSE"}</button>
                         </div>
                         <div class="selection-display">
